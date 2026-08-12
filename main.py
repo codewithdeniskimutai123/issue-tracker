88839535143a7b94310e2748eb7210cb1f94b1b9
kim <codewithkim@gmail.com>
2026-08-12 10:56:27 -0700

    edited the cors settings

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 app.middleware("http")(timer_middleware)
-app.middleware("http")(CORSMiddleware(
+
+app.add_middleware(CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
-))
+)
 
 app.include_router(issues_router)
 
